cfed15d2d53b058fa62ba45540568463e99d3252
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2026-01-23 13:52:24 +0300

    вроде готово

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,26 @@
 import '../css/style.css';
 
-console.log('Hello World!');
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.querySelector('.btn');
+  const collapse = document.querySelector('.collapse');
+
+  btn.addEventListener('click', () => {
+    if (collapse.classList.contains('show')) {
+      collapse.style.maxHeight = `${collapse.scrollHeight}px`;
+      // eslint-disable-next-line no-unused-expressions
+      collapse.offsetHeight;
+      collapse.style.maxHeight = '0';
+      collapse.classList.remove('show');
+    } else {
+      collapse.style.maxHeight = '0';
+      collapse.classList.add('show');
+      collapse.style.maxHeight = `${collapse.scrollHeight}px`;
+
+      setTimeout(() => {
+        if (collapse.classList.contains('show')) {
+          collapse.style.maxHeight = 'auto';
+        }
+      }, 1000);
+    }
+  });
+});
